3c404835f241ddd9566a5bb94390cb36259f81c1
littlepeach17 <jillian.soriano@mail.utoronto.ca>
2026-02-25 18:27:45 -0500

    Add arch, remove hard coded positions

--- js/phases_crashes.js
+++ js/phases_crashes.js
@@ -1,16 +1,25 @@
-const positions = [
-    {x: 100, y: 400},
-    {x: 210, y: 240},
-    {x: 360, y: 180},
-    {x: 510, y: 230},
-    {x: 610, y: 300},
-    {x: 710, y: 400}, // need to add more positions because there are 13 phases or i need to shorten the csv file
-]
 
 export function render(svg, data) {
+    const width = +svg.attr("width");
+    const height = +svg.attr("height");
+    const margin = 80;
+    const baseline = height - margin; // y coordinate for ground
+    const peakHeight = height * 0.6; // highest point for the arch
+
+    const cruiseIndex = data.findIndex(d => d.flight_phase.includes("Cruise"));
+
+    const xScale = d3.scaleLinear()
+       .domain([0, data.length - 1])
+        .range([margin, width - margin]);
+
+    const archScale = d3.scaleLinear()
+       .domain([0, cruiseIndex, data.length - 1])
+       .range([0, 1, 0])
+       .interpolate(d3.interpolateNumber);
+
     const radiusScale = d3.scaleSqrt()
         .domain([0, d3.max(data, d => d.crashes)])
-        .range([0, 40]);
+        .range([5, 45]);
 
     const colorScale = d3.scaleSequential(d3.interpolatePurples)
         .domain([0, d3.max(data, d => d.crashes)]);
@@ -21,9 +30,9 @@ export function render(svg, data) {
         .append("g")
         .attr("class", "phase-circle")
         .attr("transform", (d, i) => {
-            const pos = positions[i];
-            if (!pos) return `translate(0,0)`;
-            return `translate(${pos.x},${pos.y})`;
+            const x = xScale(i)
+            const y = baseline - (Math.sin(archScale(i) * Math.PI / 2) * peakHeight);
+            return `translate(${x}, ${y})`;
         });
 
     circles.append("circle")
@@ -36,13 +45,15 @@ export function render(svg, data) {
     circles.append("text")
         .text(d => d.flight_phase)
         .attr("text-anchor", "middle")
-        .attr("dy", d => radiusScale(d.crashes) + 15)
-        .style("fill", "#333");
+        .attr("dy", d => radiusScale(d.crashes) + 20)
+        .style("fill", "white");
 
     svg.append("image")
         .attr("xlink:href", "images/airplane.png")
-        .attr("x", 40)
-        .attr("y", 400);
+        .attr("x", xScale(0) - 50)
+        .attr("y", baseline - 50)
+        .attr("width", 40)
+        .attr("height", 40);
 }
 
 
