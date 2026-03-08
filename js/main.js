2b6ca6717d07046af9e4323064e2b9cfc55bfa7b
sahilhd <sahilhandauni@gmail.com>
2026-03-08 17:49:39 -0400

    weather crash info

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import * as render_jar from './jar_viz.js';
 import * as phases from './phases_crashes.js';
 import * as crash_vs_year from './crash_vs_year.js';
+import * as weather_viz from './weather_viz.js';
 
 let svg_phases = d3.select("#phases-chart")
     .append("svg")
@@ -18,6 +19,12 @@ let svg_crash_vs_year = d3.select("#crash-vs-year")
     .append("svg")
     .style("max-width", "100%")
     .attr("text-anchor", "middle");
+
+let svg_weather = d3.select("#weather-viz")
+    .append("svg")
+    .attr("width", 800)
+    .attr("height", 350)
+    .style("max-width", "100%");
 
 
 d3.csv("data/plane_crashes.csv").then(csv => {
@@ -39,4 +46,8 @@ d3.csv("data/phase_crashes.csv").then(csv => {
         d.crashes = +d.crashes;
     })
     phases.render(svg_phases, csv);
+});
+
+d3.csv("data/weather_crashes.csv").then(csv => {
+    weather_viz.render(svg_weather, csv);
 });
